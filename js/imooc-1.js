d0d07f636f65b0ba0c59e7a27716c42271aefccf
潘栋民 <1574165902@qq.com>
2017-06-19 22:43:07 +0800

    表格的变色/添加/删除

---
+++ js/imooc-1.js
@@ -0,0 +1,52 @@
+//改变表格的方法
+var changeColor = function(obj, color){
+	obj.bgColor = color ;//不知道Color为什么要大写
+}
+
+//实现删除的方法
+var deleteTr = function(obj){
+	var tr=obj.parentNode.parentNode;  //obj是a的对象，tr是tr的对象
+	var tbody=tr.parentNode;  			//表格对象
+	tbody.removeChild(tr);  
+}
+
+//实现添加的方法
+var addTr = function(){
+	var trNode = document.createElement("tr");
+	var tdNode = document.createElement("td");
+	var textNode = document.createTextNode("待填");
+	tdNode.appendChild(textNode);
+	trNode.appendChild(tdNode);
+	trNode.appendChild(tdNode);
+	trNode.appendChild(tdNode);
+	document.getElementById("mesTable").appendChild(trNode);
+}
+
+window.onload = function(){
+
+	//变色,获取全部标签，循环遍历各行，绑定事件
+	var trChangeColor = document.getElementsByTagName("tr");
+	for(var i=0; i<trChangeColor.length; i++){
+		trChangeColor[i].addEventListener("mouseover", function(){
+			changeColor(this, "#fff");
+		}, false);
+
+		trChangeColor[i].addEventListener("mouseout", function(){
+			changeColor(this, "#ddd");
+		}, false);
+	}
+
+	//删除，跟上面同理，调用删除方法
+	var trDelete = document.getElementsByTagName("a");
+	for(var i=0; i<trDelete.length; i++){
+		trDelete[i].addEventListener("click", function(){
+			deleteTr(this);
+		}, false);
+	}
+
+	//添加一行表格
+	var addButton = document.getElementById("addTr") ;
+	addButton.addEventListener("click", function(){
+		addTr();
+	}, false);
+}
